4b15ef8f28596d8ab1a606183b8fac90b84fbbbe
Tasneem Fandakli <tasnimtepe@gmail.com>
2024-01-19 15:50:03 +0300

    create resolver, service and user entity

---
+++ apps/users/src/user.resolver.ts
@@ -0,0 +1,25 @@
+import { BadRequestException } from '@nestjs/common';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+import { RegisterResponse } from './types/user.types';
+import { RegisterDto } from './dto/user.dto';
+import { Response } from 'express';
+
+@Resolver('User')
+export class UsersResolver {
+  constructor(private readonly userService: UsersService) {}
+
+  @Mutation(() => RegisterResponse)
+  async register(
+    @Args('registerDto') registerDto: RegisterDto,
+    @Context() context: { res: Response },
+  ): Promise<RegisterResponse> {
+    if (!registerDto.name || !registerDto.email || !registerDto.password) {
+      throw new BadRequestException('Please fill the all fields');
+    }
+
+    const { user } = await this.userService.register(registerDto, context.res);
+
+    return { user };
+  }
+}
--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -1,8 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { RegisterDto } from './dto/user.dto';
+import { Response } from 'express';
+import * as bcrypt from 'bcryptjs';
+import { PrismaService } from '../../../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaService) {}
+
+  // register user service
+  async register(registerDto: RegisterDto, response: Response) {
+    const { name, email, password, phone_number } = registerDto;
+
+    const isEmailExist = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (isEmailExist) {
+      throw new BadRequestException('User already exist with this email!');
+    }
+
+    const phoneNumbersToCheck = [phone_number];
+
+    const usersWithPhoneNumber = await this.prisma.user.findMany({
+      where: {
+        phone_number: {
+          not: null,
+          in: phoneNumbersToCheck,
+        },
+      },
+    });
+
+    if (usersWithPhoneNumber.length > 0) {
+      throw new BadRequestException(
+        'User already exist with this phone number!',
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        phone_number,
+      },
+    });
+
+    return { user, response };
   }
 }
